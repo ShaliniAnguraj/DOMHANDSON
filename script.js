74435d71f3dd36bd0645810e4eeda14f758fc66f
SHALINI ANGURAJ <84854866+ShaliniAnguraj@users.noreply.github.com>
2022-10-12 12:25:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function foo(){
+    var res=document.getElementById("FN").value;
+    var res1=document.getElementById("MN").value;
+    var res2=document.getElementById("LN").value;
+    var res3=document.getElementById("email").value;
+    var res4=document.getElementById("pass").value;
+     console.log(res,res1,res2,res3,res4);
+    }
