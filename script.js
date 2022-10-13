efbcf9926ee6658f2be3fad65997990615a79f34
SHALINI ANGURAJ <84854866+ShaliniAnguraj@users.noreply.github.com>
2022-10-13 12:53:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
+var table=document.createElement("table");
+table.setAttribute("class","table");
 
-function foo(){
-    var res=document.getElementById("FN").value;
-    var res1=document.getElementById("MN").value;
-    var res2=document.getElementById("LN").value;
-    var res3=document.getElementById("email").value;
-    var res4=document.getElementById("pass").value;
-     console.log(res,res1,res2,res3,res4);
-    }
+var head=document.createElement("thead");
+head.setAttribute("class","thead-dark");
+
+var trh=document.createElement("tr");
+
+var th1=document.createElement("th");
+th1.setAttribute("scope","col");
+th1.innerHtml="FIRST";
+
+var th2=document.createElement("th");
+th2.setAttribute("scope","col");
+th2.innerHtml="MIDDLE";
+
+var th3=document.createElement("th");
+th3.setAttribute("scope","col");
+th3.innerHtml="LAST";
+
+var body=document.createElement("tbody");
+
+var trb=document.createElement("tr");
+
+var tb1 = document.createElement("td");
+tb1.innerHTML="Mark";
+
+var tb2 = document.createElement("td");
+tb2.innerHTML="Otto";
+
+var tb3 = document.createElement("td");
+tb3.innerHTML="@mdo";
+
+trb.append(tb1,tb2,tb3);
+body.append(trb);
+
+trh.append(th1,th2,th3);
+head.append(trh);
+
+table.append(head,body);
+document.body.append(table);
+
+
+
+
+
+
+
+
+
